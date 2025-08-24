c03aad0bb53e4c3f2a6fd802ec0b4b006cc45ce5
Bernhard Posselt <bernhard@posselt.at>
2025-08-24 14:34:52 +0200

    fix scripts

--- package.sh
+++ package.sh
@@ -14,7 +14,7 @@ mkdir -p build/pf2e-return-of-the-runelords-conversion/
 # create archive
 node ./scripts/update-module-json.mjs "$version"
 cp module.json README.md OpenGameLicense.md LICENSE ORC.md CHANGELOG.md build/pf2e-return-of-the-runelords-conversion/
-cp -r packs/ assets/ build/pf2e-return-of-the-runelords-conversion/
+cp -r packs/ build/pf2e-return-of-the-runelords-conversion/
 
 cd build
 zip -r release.zip pf2e-return-of-the-runelords-conversion
